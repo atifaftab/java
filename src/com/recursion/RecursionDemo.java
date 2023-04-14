fa071b0aefa191dff7a36f9e8a2cefa75a3ac437
Atif <iamatif96@gmail.com>
2023-04-14 15:57:12 +0800

    recursion Demo

---
+++ src/com/recursion/RecursionDemo.java
@@ -0,0 +1,16 @@
+package com.recursion;
+
+public class RecursionDemo {
+
+    public static void main(String[] args) {
+        int n =5;
+        recursionMethod(5);
+
+    }
+    static void recursionMethod(int num){
+        if(num==0)
+            return;
+        System.out.println(num);
+        recursionMethod(num-1);
+    }
+}
