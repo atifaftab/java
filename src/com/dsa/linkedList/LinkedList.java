6200351cc5782780f50b45e1f7b6693504f36b35
Atif <iamatif96@gmail.com>
2023-05-04 16:19:54 +0800

    linkedlist methods

--- src/com/dsa/linkedList/LinkedList.java
+++ src/com/dsa/linkedList/LinkedList.java
@@ -5,27 +5,40 @@ public class LinkedList {
     private Node tail;
     private int length;
 
-    class Node{
+    class Node {
         int value;
         Node next;
 
-        Node(int value){
+        Node(int value) {
             this.value = value;
         }
 
     }
-    LinkedList(int value){
+
+    LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    public void printList(){
+    public void printList() {
         Node temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.println(temp.value);
             temp = tail.next;
         }
+    }
+
+    public void getHead() {
+        System.out.println("Head " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length " + length);
     }
 }
