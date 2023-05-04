a215de43aae8e86513e5b3675a2b2f436f3d5450
Atif <iamatif96@gmail.com>
2023-05-04 17:21:47 +0800

    linkedlist Append

--- src/com/dsa/linkedList/append/AppEndLinkedList.java
+++ src/com/dsa/linkedList/append/AppEndLinkedList.java
@@ -1,56 +1,47 @@
-package com.dsa.linkedList;
+package com.dsa.linkedList.append;
 
-public class LinkedList {
+public class AppEndLinkedList {
+
     private Node head;
     private Node tail;
     private int length;
 
-    class Node {
+
+    class Node{
         int value;
         Node next;
 
-        Node(int value) {
+        Node(int value){
             this.value = value;
         }
 
     }
-
-    LinkedList(int value) {
+    AppEndLinkedList(int value){
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
         length = 1;
     }
 
-    public void printList() {
+    public void displayLinkedList(){
         Node temp = head;
-        while (temp != null) {
+        while(temp != null){
             System.out.println(temp.value);
             temp = temp.next;
         }
     }
 
-    public void getHead() {
-        System.out.println("Head " + head.value);
-    }
 
-    public void getTail() {
-        System.out.println("Tail " + tail.value);
-    }
-
-    public void getLength() {
-        System.out.println("Length " + length);
-    }
-
-    public void append(int value) {
+    public void appEnd(int value){
         Node newNode = new Node(value);
-        if (length == 0) {
+
+        if(length == 0){
             head = newNode;
             tail = newNode;
-        } else {
+        }
+        else{
             tail.next = newNode;
             tail = newNode;
-
         }
         length++;
     }
---
+++ src/com/dsa/linkedList/append/Main.java
@@ -0,0 +1,15 @@
+package com.dsa.linkedList.append;
+
+import com.dsa.linkedList.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AppEndLinkedList linkedList = new AppEndLinkedList(1);
+
+        linkedList.appEnd(2);
+
+        linkedList.displayLinkedList();
+
+    }
+}
