ade04f751c2ec73ed8bbcded27b92e791383f798
Atif <iamatif96@gmail.com>
2023-05-11 21:36:06 +0800

    Prepend done

---
+++ src/com/dsa/linkedList/prepend/MainDriver.java
@@ -0,0 +1,18 @@
+package com.dsa.linkedList.prepend;
+
+public class MainDriver {
+
+    public static void main(String[] args) {
+
+        PrepEndLinkedList myLinkedList = new PrepEndLinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.removeNode();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.displayLinkedList();
+
+        myLinkedList.prepEnd(3);
+        System.out.println("\nLinked List after append :");
+        myLinkedList.displayLinkedList();
+    }
+}
---
+++ src/com/dsa/linkedList/prepend/PrepEndLinkedList.java
@@ -0,0 +1,78 @@
+package com.dsa.linkedList.prepend;
+
+public class PrepEndLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    PrepEndLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void displayLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public Node removeNode() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        pre.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+
+    }
+
+    public void prepEnd(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+
+
+    }
+}
