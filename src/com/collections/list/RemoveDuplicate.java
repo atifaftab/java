d95d695f87ef2b51385b39377556667b3a1ac728
Atif <iamatif96@gmail.com>
2023-06-06 06:39:43 +0800

    remove duplicate from an array

---
+++ src/com/collections/list/RemoveDuplicate.java
@@ -0,0 +1,23 @@
+package com.collections.list;
+
+import java.util.*;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+
+        List<Integer> l1 = Arrays.asList(1,2,3,4,5,1,2,4,9);
+//        List<Integer> list = new ArrayList<>();
+//        list.addAll(l1);
+//
+//        //naive method (creating linkedHashset from list
+//        Set<Integer> set = new LinkedHashSet<>();
+//        set.addAll(list);
+//        list.clear();
+//        list.addAll(set);
+//
+//        System.out.println(list);
+
+        //using distinct
+        System.out.println(l1.stream().distinct().toList());
+    }
+}
