ad473494afeb5fe9fa7df0c4348b9bc4543b8a16
Atif <iamatif96@gmail.com>
2023-06-05 17:06:42 +0800

    number is sum of 2 prime number

--- src/com/numbers/NumSumOf2PrimeNum.java
+++ src/com/numbers/NumSumOf2PrimeNum.java
@@ -1,11 +1,11 @@
 package com.numbers;
-
+//number is sum of 2 prime number
 public class NumSumOf2PrimeNum {
     public static void main(String[] args) {
-        int num = 34;
+        int num = 16;
         boolean flag = false;
 
-        for (int i = 2; i <= num / 2; i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (checkPrime(i)) {
                 if (checkPrime(num - i)) {
                     System.out.println(num + " is sum of prime numbers " + i + " and " + (num - i));
