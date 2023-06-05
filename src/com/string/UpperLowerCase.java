69f92cc5bba726bb9cc2aa82f120a5a6ac9fb13c
Atif <iamatif96@gmail.com>
2023-06-05 18:11:39 +0800

    using for each loop

--- src/com/string/UpperLowerCase.java
+++ src/com/string/UpperLowerCase.java
@@ -11,15 +11,24 @@ public class UpperLowerCase {
         String up = "";
         String lo = "";
 
-        for(int i=0; i< s.length(); i++){
-            if(Character.isUpperCase(s.charAt(i))){
-                up = up + s.charAt(i);
-            }
-            if(Character.isLowerCase(s.charAt(i))){
-                lo = lo + s.charAt(i);
-            }
+//        for(int i=0; i< s.length(); i++){
+//            if(Character.isUpperCase(s.charAt(i))){
+//                up = up + s.charAt(i);
+//            }
+//            if(Character.isLowerCase(s.charAt(i))){
+//                lo = lo + s.charAt(i);
+//            }
+//
+//        }
 
+        for( char c: s.toCharArray()){
+            if(Character.isUpperCase(c))
+                up = up + c;
+            if(Character.isLowerCase(c))
+                lo = lo + c;
         }
+
+
         System.out.println("after "+up + lo);
     }
 }
