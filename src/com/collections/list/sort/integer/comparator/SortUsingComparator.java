de29f2ae7f36eb374fdc448364f9c84abfda32d7
Atif <iamatif96@gmail.com>
2023-06-10 20:51:58 +0800

    sorting Integer by using Comparator

---
+++ src/com/collections/list/sort/integer/comparator/SortUsingComparator.java
@@ -0,0 +1,31 @@
+package com.collections.list.sort.integer.comparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortUsingComparator {
+    public static void main(String[] args) {
+
+        Comparator<Integer> com = new Comparator<Integer>(){
+            @Override
+            public int compare(Integer i, Integer j){
+                if(i > j)
+                    return 1;
+                else return -1;
+            }
+        };
+
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(32);
+        list.add(36);
+        list.add(4);
+        list.add(0);
+
+        Collections.sort(list, com);
+        System.out.println(list);
+    }
+
+}
