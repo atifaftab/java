6cdac024f8f89581d6d797b77d66745980ffb84e
Atif <iamatif96@gmail.com>
2023-06-21 15:34:52 +0800

    streams

---
+++ src/com/java8/streams/transaction/StreamExample.java
@@ -0,0 +1,11 @@
+package com.java8.streams.transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamExample {
+    public static void main(String[] args) {
+
+        List<Transaction> list = new ArrayList<>();
+    }
+}
