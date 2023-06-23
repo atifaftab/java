b764e1f36a5bfc602dada5388018609b47c60211
Atif <iamatif96@gmail.com>
2023-06-23 17:53:43 +0800

    a return true if one of argument is 1 or sum of both argument is 1

---
+++ src/com/codingGame/Test1.java
@@ -0,0 +1,15 @@
+package com.codingGame;
+// a return true if one of argument is 1 or sum of both argument is 1
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println(a(-3,4));
+    }
+
+    static boolean a(int i, int j){
+        if(i ==1 || j == 1) return true;
+        if( i + j == 1) return true;
+        return false;
+    }
+}
