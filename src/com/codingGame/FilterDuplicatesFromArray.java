cb46da6f3245f12776220f3ab39cdb62e248f91b
Atif <iamatif96@gmail.com>
2023-06-24 09:23:08 +0800

    Filter Duplicates From Array class using set and list

--- src/com/codingGame/FilterDuplicatesFromArray.java
+++ src/com/codingGame/FilterDuplicatesFromArray.java
@@ -1,5 +1,28 @@
 package com.codingGame;
 
+import java.util.*;
+
+//returns containing the values of input array without duplicate, initial order must be kept
 public class FilterDuplicatesFromArray {
 
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 8, 2, 7, 3, 9, 6, 9, 1, 4, 2};
+        System.out.println(Arrays.toString(filterDuplicates(arr)));
+    }
+
+    public static int[] filterDuplicates(int[] arr) {
+
+        Set<Integer> unique = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+
+        for (int n : arr) {
+            if (!unique.add(n)) {
+                list.add(n);
+            }
+        }
+//        System.out.println(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
+
+    }
 }
