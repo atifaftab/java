9a036dd8c7a7f286ea4d689a663d06a4cb866520
Atif <iamatif96@gmail.com>
2024-02-16 12:45:03 +0400

    array demo

--- src/com/arrays/ArrayDemo.java
+++ src/com/arrays/ArrayDemo.java
@@ -1,13 +1,21 @@
 package com.arrays;
 
+import java.util.Arrays;
+
 public class ArrayDemo {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
 
-        for (int i=0; i<arr.length;i++){
-            System.out.print(i+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
+        System.out.println("\n------------");
 
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n------------");
+        System.out.println(Arrays.toString(arr));
 //        System.out.println(arr.toString());
     }
 }
