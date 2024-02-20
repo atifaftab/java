00de5e92b1453d9fbdd2d7fbea2c70defd4c4625
Atif <iamatif96@gmail.com>
2024-02-20 22:26:21 +0400

    missing number

--- src/com/arrays/MissingNoInArray.java
+++ src/com/arrays/MissingNoInArray.java
@@ -4,17 +4,28 @@ import java.util.Arrays;
 
 public class MissingNoInArray {
     public static void main(String[] args) {
-        int[] arr = {1,5,3,2};
-        int n=arr.length;
+        //find the missing number in the array
+        int[] arr = {1, 5, 3, 2};
+        int n = arr.length;
+//        System.out.println("n = " + n);
+//        System.out.println(Arrays.stream(arr).sum());
         Arrays.sort(arr);
-        int b=1;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==b)
-            {
+        int b = 1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == b) {
                 b++;
             }
         }
         System.out.println(b);
+
+        System.out.println("---------");
+
+//        Arrays.stream(arr).
+
+        int sum = ((n + 1) * (n + 2)) / 2;
+        for (int j : arr) {
+            sum -= j;
+        }
+        System.out.println("the missing number is " + sum);
     }
 }
