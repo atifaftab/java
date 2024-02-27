bfa2d2a95c77a5d17a9a21001cca5e404dbdee61
Atif <iamatif96@gmail.com>
2024-02-27 22:53:34 +0400

    append main

---
+++ src/com/dsa/linkedListPractice/append/MainLinkedListClass.java
@@ -0,0 +1,9 @@
+package com.dsa.linkedListPractice.append;
+
+public class MainLinkedListClass {
+    public static void main(String[] args) {
+        LinkedListExample linkedListExample = new LinkedListExample(1);
+        linkedListExample.appEnd(2);
+        linkedListExample.printLinkedList();
+    }
+}
