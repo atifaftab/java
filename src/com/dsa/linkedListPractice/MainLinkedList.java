c25d515d892c7b22548f84a4defff0a341547205
Atif <iamatif96@gmail.com>
2024-02-27 22:27:28 +0400

    LinkedList main Example

---
+++ src/com/dsa/linkedListPractice/MainLinkedList.java
@@ -0,0 +1,12 @@
+package com.dsa.linkedListPractice;
+
+public class MainLinkedList {
+    public static void main(String[] args) {
+
+        LinkedListExample linkedListExample = new LinkedListExample(5);
+        linkedListExample.printLinkedList();
+        linkedListExample.printHead();
+        linkedListExample.printTail();
+    }
+
+}
