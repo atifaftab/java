5dee4b23819c592f88fe68749e369b4b7ff5ed08
Atif <iamatif96@gmail.com>
2024-02-27 22:19:43 +0400

    find an element in an array in O(n/2) time complexity

--- src/com/arrays/findElementInShortTime.java
+++ src/com/arrays/findElementInShortTime.java
@@ -10,16 +10,41 @@ public class findElementInShortTime {
         int element = 54;
         element = in.nextInt();
         int k = arrNum.length - 1;
-        for (int i = 0; i < arrNum.length / 2; i++) {
+
+        //using 2 element
+//        for (int i = 0; i < arrNum.length / 2; i++) {
+//            if (element == arrNum[i]) {
+//                System.out.println(arrNum[i]);
+//                break;
+//            }
+//            if (element == arrNum[k]) {
+//                System.out.println(arrNum[k]);
+//                break;
+//            }
+//            k--;
+//        }
+
+        //using 3 element
+        int x = 1;
+        int y = 2;
+        for (int i = 0; i < arrNum.length - 1; i = i + 3) {
             if (element == arrNum[i]) {
+                System.out.println("i = " + i);
                 System.out.println(arrNum[i]);
                 break;
             }
-            if (element == arrNum[k]) {
-                System.out.println(arrNum[k]);
+            if (element == arrNum[x]) {
+                System.out.println("x = " + x);
+                System.out.println(arrNum[x]);
                 break;
             }
-            k--;
+            if (element == arrNum[y]) {
+                System.out.println("y = " + y);
+                System.out.println(arrNum[y]);
+                break;
+            }
+            x = x + 3;
+            y = y + 3;
         }
     }
 }
