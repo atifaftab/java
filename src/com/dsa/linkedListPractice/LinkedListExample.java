5ec5dfbddd28724d32eac4eb34ccdbaf4ad5c443
Atif <iamatif96@gmail.com>
2024-02-27 21:43:42 +0400

    linkedList practice

---
+++ src/com/dsa/linkedListPractice/LinkedListExample.java
@@ -0,0 +1,18 @@
+package com.dsa.linkedListPractice;
+
+public class LinkedListExample {
+
+    Node tail;
+    Node head;
+
+
+    static class Node{
+        int value;
+        Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+    }
+}
