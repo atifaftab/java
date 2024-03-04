c3d3e1b51c46a80543c4b3e98aabebba9dea09eb
Atif <iamatif96@gmail.com>
2024-03-04 23:36:47 +0800

    2d array example using Arrays.toString

--- src/com/arrays/twoDorMatrix/Print2D.java
+++ src/com/arrays/twoDorMatrix/Print2D.java
@@ -1,11 +1,11 @@
 package com.arrays.twoDorMatrix;
 
+import java.util.Arrays;
+
 public class Print2D {
     //multi dimentional array
     public static void main(String[] args) {
-        int mat[][] = {{1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 10, 11, 12}};
+        int mat[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
@@ -19,7 +19,10 @@ public class Print2D {
                 System.out.print(j + " ");
             }
         }
-        System.out.println("--------");
-
+        System.out.println("\n--------");
+        // using Arrays.toString
+        for (int[] arr : mat) {
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
