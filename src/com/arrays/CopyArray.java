8e3ffa940a954036aa53b402cd08bd22667af117
Atif <iamatif96@gmail.com>
2024-03-06 18:23:49 +0800

    copy array

---
+++ src/com/arrays/CopyArray.java
@@ -0,0 +1,30 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 53, 76, 2, 0};
+        System.out.println("original array");
+        System.out.println(Arrays.toString(arr));
+
+        //traditional method
+        int[] copyArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            copyArr[i] = arr[i];
+        }
+        System.out.println(Arrays.toString(copyArr));
+
+        //System,arraycopy
+        int[] copyArr1 = new int[arr.length];
+        System.arraycopy(arr, 0, copyArr1, 0, arr.length);
+        System.out.println(Arrays.toString(copyArr1));
+
+
+        //using clone
+        int[] copyArr2 = arr.clone();
+        System.out.println(Arrays.toString(copyArr2));
+
+//        Arrays.copyOf();
+    }
+}
