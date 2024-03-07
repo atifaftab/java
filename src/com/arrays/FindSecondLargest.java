455d0bcfd1babe0fc85a780b6991f584bfe66ae5
Atif <iamatif96@gmail.com>
2024-03-07 18:37:32 +0800

    using sort

--- src/com/arrays/FindSecondLargest.java
+++ src/com/arrays/FindSecondLargest.java
@@ -1,7 +1,28 @@
 package com.arrays;
 
+import java.util.Arrays;
+
 public class FindSecondLargest {
     public static void main(String[] args) {
+        int[] arr = {-1, 87, 343, 233, 0, -934};
+        //normal way
+        int max = arr[0];
+        int main = arr[0];
+        for (int n : arr) {
+            max = Math.max(n, max);
+        }
+        System.out.println("largest = " + max);
+        for (int n : arr) {
+            if (n < max && n > main) {
+                main = n;
+            }
+        }
+        System.out.println("2nd largest = " + main);
+
+        //by sorting
+        Arrays.sort(arr);
+        System.out.println("largest " + arr[arr.length - 1]);
+        System.out.println("2nd largest " + arr[arr.length - 2]);
 
     }
 }
