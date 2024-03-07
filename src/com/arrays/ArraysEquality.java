e40e6e1a9bede3366351eaabf07c5af3a55df258
Atif <iamatif96@gmail.com>
2024-03-07 15:55:50 +0800

    arrays equality

--- src/com/arrays/ArraysEquality.java
+++ src/com/arrays/ArraysEquality.java
@@ -7,6 +7,21 @@ public class ArraysEquality {
         int[] a = {30, 25, 40};
         int[] b = {30, 25, 40};
 
+        boolean flag = false;
+        if (a.length != b.length) {
+            flag = false;
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[i]) {
+                    flag = false;
+                } else {
+                    flag = true;
+                }
+            }
+        }
+        System.out.println(flag);
+
+
         if (Arrays.equals(a, b)) {
             System.out.println("both arrays are equals");
             return;
