581c44a88a364628cd1ab01251d47ca272410177
Atif <iamatif96@gmail.com>
2024-03-09 00:39:32 +0800

    new way

--- src/com/arrays/RemoveAllOccurrence.java
+++ src/com/arrays/RemoveAllOccurrence.java
@@ -1,19 +1,23 @@
 package com.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RemoveAllOccurrence {
     public static void main(String[] args) {
         //remove all occurrence of an element from array
         int[] arr = {3, 9, 2, 3, 1, 7, 2, 3, 5};
         System.out.println(Arrays.toString(arr));
-        int[] copyArr = new int[arr.length];
+//        int[] copyArr = new int[arr.length];
+        List<Integer> list = new ArrayList<>();
         int num = 3;
         for (int i = 0; i < arr.length; i++) {
             if (num != arr[i]) {
-                copyArr[i] = arr[i];
+//                copyArr[i] = arr[i];
+                list.add(arr[i]);
             }
         }
-        System.out.println(Arrays.toString(copyArr));
+        System.out.println(list);
     }
 }
