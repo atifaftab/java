5d5b53e94737411a0d571d45dafa29a2998ac6a9
Atif <iamatif96@gmail.com>
2024-03-12 16:35:52 +0800

    final keyword example

---
+++ src/com/keyword/finalExample/Parent.java
@@ -0,0 +1,19 @@
+//package com.keyword.finalExample;
+//
+//public class Parent {
+//    public final void print() {
+//        System.out.println("true = " + true);
+//    }
+//}
+//
+//class Child extends Parent {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    @Override
+//    public void print() {
+//        System.out.println("true = " + true);
+//    }
+//
+//}
