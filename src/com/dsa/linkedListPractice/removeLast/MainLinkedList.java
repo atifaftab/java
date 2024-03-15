39c930b579a54beff65b1475bb70dd112e2c61f5
Atif <iamatif96@gmail.com>
2024-03-15 19:15:50 +0800

    remove Node from last

--- src/com/dsa/linkedListPractice/removeLast/MainLinkedList.java
+++ src/com/dsa/linkedListPractice/removeLast/MainLinkedList.java
@@ -2,8 +2,19 @@ package com.dsa.linkedListPractice.removeLast;
 
 public class MainLinkedList {
     public static void main(String[] args) {
-        LinkedListLastRemoved linkedListLastRemoved = new LinkedListLastRemoved(5);
-        linkedListLastRemoved.appEnd(4);
+        LinkedListLastRemoved linkedListLastRemoved = new LinkedListLastRemoved(1);
+        linkedListLastRemoved.appEnd(2);
+//        linkedListLastRemoved.appEnd(3);
+//        linkedListLastRemoved.appEnd(4);
         linkedListLastRemoved.printLinkedList();
+        System.out.println("------- after removing --------");
+
+        //returns 2
+        System.out.println(linkedListLastRemoved.removeLast().value);
+        //returns 1
+        System.out.println(linkedListLastRemoved.removeLast().value);
+        //returns null
+        System.out.println(linkedListLastRemoved.removeLast());
+
     }
 }
