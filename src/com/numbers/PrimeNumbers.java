95bd026053964a60c219ec93287e544678f67d0e
Atif <iamatif96@gmail.com>
2024-03-19 21:55:23 +0800

    prime number using lambda

--- src/com/numbers/PrimeNumbers.java
+++ src/com/numbers/PrimeNumbers.java
@@ -1,19 +1,34 @@
 package com.numbers;
 
+import java.util.stream.IntStream;
+
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int num = 13;
-        if(primeNumber(num))
-            System.out.println(num +" is prime");
+        int num = 2;
+        if (primeNo(num))
+            System.out.println(num + " is prime");
         else System.out.println(num + " is not prime");
     }
 
-    private static boolean primeNumber(int num){
+    private static boolean primeNumber(int num) {
         if (num <= 1) return false;
-        for(int i=2; i<= Math.sqrt(num); i++){
-            if(num % i == 0)
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
                 return false;
         }
         return true;
+    }
+
+    private static boolean primeNum(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % 2 == 0) return true;
+        }
+        return false;
+    }
+
+    //using lambda
+    private static boolean primeNo(int num) {
+        return num > 1 && IntStream.range(2, (int) Math.sqrt(num)).noneMatch(i -> num % i == 0);
     }
 }
