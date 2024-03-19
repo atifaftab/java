a215b9d58ed6c826b68c435e81c9132e4b4f1572
Atif <iamatif96@gmail.com>
2024-03-19 18:24:03 +0800

    builder design pattern

---
+++ src/com/builder/Main.java
@@ -0,0 +1,12 @@
+package com.builder;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = Person.builder()
+                .name("Atif")
+                .age(28)
+                .address(new Address("add"))
+                .build();
+        System.out.println("person = " + person);
+    }
+}
