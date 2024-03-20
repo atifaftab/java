e6e18dff350398502420f439818c795fddfcb9d0
Atif <iamatif96@gmail.com>
2024-03-20 18:38:17 +0800

    swap number

---
+++ src/com/numbers/SwapNumbers.java
@@ -0,0 +1,17 @@
+package com.numbers;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Swap without using a temporary variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
