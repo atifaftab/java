d485e6837e67ed7c6ee1f955caac44693891911e
Atif <iamatif96@gmail.com>
2024-03-22 00:54:13 +0800

    remove first node

---
+++ src/com/dsa/linkedList/removeFirst/RemoveFirstLinkedList.java
@@ -0,0 +1,31 @@
+package com.dsa.linkedList.removeFirst;
+
+public class RemoveFirstLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    RemoveFirstLinkedList(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void printLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+}
