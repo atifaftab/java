68973962a0019c20315e3d6c1d743ff74b478fc1
Atif <iamatif96@gmail.com>
2024-03-21 11:10:46 +0800

    SortIgnoringSpaces

--- src/com/string/SortIgnoringSpaces.java
+++ src/com/string/SortIgnoringSpaces.java
@@ -1,5 +1,8 @@
 package com.string;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SortIgnoringSpaces {
     public static void main(String[] args) {
         /*
@@ -7,5 +10,12 @@ public class SortIgnoringSpaces {
           sort a given array of Strings in alphabetical order
           ignoring spaces (' ' symbols) within the strings.
          */
+        sortIgnoringSpaces(new String[]{" ", "test", "ABC", "why", "    ", "HLB", "webiste", "google", "1", "9", "-111"});
+    }
+
+    public static void sortIgnoringSpaces(String[] array) {
+        Arrays.sort(array, Comparator.comparing((String s) -> s.replace(" ", "")));
+        System.out.println(Arrays.toString(array));
+
     }
 }
