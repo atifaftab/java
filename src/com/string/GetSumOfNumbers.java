2c8892d54b6e82df4ae023a1eab4da184942ada0
Atif <iamatif96@gmail.com>
2024-03-21 10:21:28 +0800

    sum of all integers in string separated by space

---
+++ src/com/string/GetSumOfNumbers.java
@@ -0,0 +1,18 @@
+package com.string;
+
+public class GetSumOfNumbers {
+    public static void main(String[] args) {
+        /*
+          Please implement this method to
+          calculate the sum of all integers found in the parameter String. You can assume that
+          integers are separated from other parts with one or more spaces (' ' symbol).
+          For example, s="12 some text 3  7", result: 22 (12+3+7=22)
+         */
+        String s="12 some text 3  7";
+        getSumOfNumbers(s);
+
+    }
+    public static void getSumOfNumbers(String s){
+
+    }
+}
