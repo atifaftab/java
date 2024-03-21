2d297f4ca939fad8a2d694c24714f7337cb57aa7
Atif <iamatif96@gmail.com>
2024-03-22 00:54:17 +0800

    remove first node

---
+++ src/com/dsa/linkedList/removeFirst/RemoveFirstMain.java
@@ -0,0 +1,8 @@
+package com.dsa.linkedList.removeFirst;
+
+public class RemoveFirstMain {
+    public static void main(String[] args) {
+        RemoveFirstLinkedList removeFirstLinkedList = new RemoveFirstLinkedList(1);
+        removeFirstLinkedList.printLinkedList();
+    }
+}
