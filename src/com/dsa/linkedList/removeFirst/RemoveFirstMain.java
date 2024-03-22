3d5980a8dbfade9396129fc3183bdb273610a448
Atif <iamatif96@gmail.com>
2024-03-22 21:56:51 +0800

    remove first node : get

--- src/com/dsa/linkedList/removeFirst/RemoveFirstMain.java
+++ src/com/dsa/linkedList/removeFirst/RemoveFirstMain.java
@@ -25,5 +25,15 @@ public class RemoveFirstMain {
         System.out.println("removed element : " + removeFirstLinkedList.removeFirst().value);
         System.out.println("removed element : " + removeFirstLinkedList.removeFirst());
         removeFirstLinkedList.getLength();
+
+        System.out.println("------ adding new elements ------");
+        removeFirstLinkedList.prepEnd(2);
+        removeFirstLinkedList.prepEnd(1);
+        removeFirstLinkedList.prepEnd(0);
+        removeFirstLinkedList.appEnd(3);
+        removeFirstLinkedList.printLinkedList();
+        removeFirstLinkedList.getLength();
+
+        System.out.println("index at 2 : " + removeFirstLinkedList.get(2).value);
     }
 }
