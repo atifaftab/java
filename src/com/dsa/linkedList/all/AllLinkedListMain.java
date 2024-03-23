597600b02ecb53927f4433667cc639c596cc242d
Atif <iamatif96@gmail.com>
2024-03-23 16:08:58 +0800

    all linkedList

---
+++ src/com/dsa/linkedList/all/AllLinkedListMain.java
@@ -0,0 +1,51 @@
+package com.dsa.linkedList.all;
+
+public class AllLinkedListMain {
+    public static void main(String[] args) {
+        AllLinkedList allLinkedList = new AllLinkedList(3);
+        allLinkedList.prepEnd(2);
+        allLinkedList.prepEnd(1);
+        allLinkedList.appEnd(4);
+        allLinkedList.appEnd(5);
+        allLinkedList.printLinkedList();
+        allLinkedList.getLength();
+
+        System.out.println("------after removing last element ------");
+
+        System.out.println("removed element : " + allLinkedList.removeLast().value);
+        System.out.println("removed element : " + allLinkedList.removeLast().value);
+        allLinkedList.getLength();
+
+        System.out.println("------ rest of the element ------");
+        allLinkedList.printLinkedList();
+        allLinkedList.getLength();
+        System.out.println("---removing first -------");
+        System.out.println("removed element : " + allLinkedList.removeFirst().value);
+        System.out.println("removed element : " + allLinkedList.removeFirst().value);
+        System.out.println("removed element : " + allLinkedList.removeFirst().value);
+        System.out.println("removed element : " + allLinkedList.removeFirst());
+        allLinkedList.getLength();
+
+        System.out.println("------ adding new elements ------");
+        allLinkedList.prepEnd(2);
+        allLinkedList.prepEnd(1);
+        allLinkedList.prepEnd(0);
+        allLinkedList.appEnd(3);
+        allLinkedList.printLinkedList();
+        allLinkedList.getLength();
+
+        System.out.println("index at 2 : " + allLinkedList.get(2).value);
+
+        System.out.println(allLinkedList.set(1, 5));
+        allLinkedList.printLinkedList();
+        System.out.println("-----re arranging ----");
+        allLinkedList.set(1, 1);
+        allLinkedList.printLinkedList();
+
+        System.out.println("-----adding 4 at index 2 ----");
+        System.out.println(allLinkedList.insertSecondMethod(0, 4));
+        allLinkedList.printLinkedList();
+
+
+    }
+}
