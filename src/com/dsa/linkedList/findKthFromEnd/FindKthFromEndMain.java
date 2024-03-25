8332d03ee31b79f594a885a5b8065c25bba40406
Atif <iamatif96@gmail.com>
2024-03-25 17:57:00 +0800

    find the kth Node from end of LikedList

---
+++ src/com/dsa/linkedList/findKthFromEnd/FindKthFromEndMain.java
@@ -0,0 +1,13 @@
+package com.dsa.linkedList.findKthFromEnd;
+
+public class FindKthFromEndMain {
+    public static void main(String[] args) {
+        FindKthFromEnd findKthFromEnd = new FindKthFromEnd(1);
+        findKthFromEnd.append(2);
+        findKthFromEnd.append(3);
+        findKthFromEnd.append(4);
+        findKthFromEnd.append(5);
+
+        System.out.println(findKthFromEnd.findKthFromEnd(-2));
+    }
+}
