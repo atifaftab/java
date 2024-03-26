3bef39f1c7b6b6289a1be04312e609755b07b157
Atif <iamatif96@gmail.com>
2024-03-27 02:25:05 +0800

    simple min using java 8

---
+++ src/com/collections/list/FindMinimum.java
@@ -0,0 +1,21 @@
+package com.collections.list;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class FindMinimum {
+    public static void main(String[] args) {
+        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        int min = num.get(0);
+        for (int i = 0; i < num.size(); i++) {
+            if (num.get(i) < min)
+                min = num.get(i);
+        }
+        System.out.println("min = " + min);
+
+
+        Optional<Integer> min1 = num.stream().min(Integer::compareTo);
+        min1.ifPresent(System.out::println);
+    }
+}
