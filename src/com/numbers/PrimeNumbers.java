e01711c92960bf122af4707beeecb6ae800db99b
Atif <iamatif96@gmail.com>
2024-03-31 12:11:27 +0800

    prime, removed extra number

--- src/com/numbers/PrimeNumbers.java
+++ src/com/numbers/PrimeNumbers.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        int num = 2;
+        int num = 97;
         if (primeNo(num))
             System.out.println(num + " is prime");
         else System.out.println(num + " is not prime");
@@ -19,16 +19,10 @@ public class PrimeNumbers {
         return true;
     }
 
-    private static boolean primeNum(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % 2 == 0) return true;
-        }
-        return false;
-    }
-
     //using lambda
     private static boolean primeNo(int num) {
         return num > 1 && IntStream.range(2, (int) Math.sqrt(num)).noneMatch(i -> num % i == 0);
+
+//        num>1 && IntStream.range(2, (int)Math.sqrt(num)).noneMatch(i -> num %==0);
     }
 }
