4f6d33e56222ae2f0e5cdb0fbaa1c94d704dc664
Atif <iamatif96@gmail.com>
2024-04-05 08:45:31 +0800

    formatting

--- src/com/java17/SwitchWithPattern.java
+++ src/com/java17/SwitchWithPattern.java
@@ -3,9 +3,9 @@ package com.java17;
 public class SwitchWithPattern {
     public static void main(String[] args) {
         int day = 7;
-        String message = switch (day){
-            case 1,7 -> "Weekend";
-            case 2,3,4,5,6 -> "Weekday";
+        String message = switch (day) {
+            case 1, 7 -> "Weekend";
+            case 2, 3, 4, 5, 6 -> "Weekday";
             default -> "Invalid";
         };
         System.out.println("message = " + message);
