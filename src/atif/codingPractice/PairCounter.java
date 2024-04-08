1b291baf5c238c24a086e71a117c2bbb2ff3cfe6
Atif <iamatif96@gmail.com>
2024-04-08 21:14:36 +0800

    code improvement

--- src/atif/codingPractice/PairCounter.java
+++ src/atif/codingPractice/PairCounter.java
@@ -9,12 +9,12 @@ public class PairCounter {
     public static void main(String[] args) {
 //        int[] array = {1, 1, 2, 2, 3, 3, 3, 1, 1, 4, 4, 4, 4}; // example array
         int[] array = {1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 33}; // example array
-        Map<Integer, Integer> counts = new HashMap<>();
+        Map<Integer, Integer> mapCounts = new HashMap<>();
         for (int num : array) {
-            counts.put(num, counts.getOrDefault(num, 0) + 1);
+            mapCounts.put(num, mapCounts.getOrDefault(num, 0) + 1);
         }
         int groupCount = 0;
-        for (int count : counts.values()) {
+        for (int count : mapCounts.values()) {
             groupCount += (count + 1) / 2;
         }
         System.out.println("Number of groups: " + groupCount);
