75fa2612760e1d474e7f78f8aa43ed1e4bfbadb6
Atif <iamatif96@gmail.com>
2024-04-08 23:57:22 +0800

    using Collectors.minBy

--- src/atif/collections/list/RemoveDuplicate.java
+++ src/atif/collections/list/RemoveDuplicate.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class RemoveDuplicate {
     public static void main(String[] args) {
 
-        List<Integer> l1 = Arrays.asList(1,2,3,4,5,1,2,4,9);
+        List<Integer> l1 = Arrays.asList(1, 2, 3, 4, 5, 1, 2, 4, 9, 3, 3, 3);
 //        List<Integer> list = new ArrayList<>();
 //        list.addAll(l1);
 //
@@ -20,5 +20,7 @@ public class RemoveDuplicate {
         //using distinct
         System.out.println(l1.stream().distinct().toList());
 
+        Set<Integer> set = new HashSet<>();
+        System.out.println(l1.stream().filter(i -> !set.add(i)).toList());
     }
 }
