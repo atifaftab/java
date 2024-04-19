fe9effe22ad509996ba8470735ae5afd8db2075a
Atif <iamatif96@gmail.com>
2024-04-19 22:19:31 +0800

    comments

--- src/atif/numbers/FibonaciByRecursion.java
+++ src/atif/numbers/FibonaciByRecursion.java
@@ -4,7 +4,8 @@ public class FibonaciByRecursion {
     public static void main(String[] args) {
 
         int num = 10;
-        System.out.println(fibonaci(num));
+//        System.out.println(fibonaci(num));
+        System.out.println(fibonaciSeries(10));
     }
 
     public static int fibonaci(int num) {
@@ -12,6 +13,12 @@ public class FibonaciByRecursion {
             return num;
         }
         return fibonaci(num - 1) + fibonaci(num - 2);
+    }
+
+    public static int fibonaciSeries(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;     // fibonaci for 2 is also 1.   0 + 1  = 1
+        return fibonaciSeries(n - 1) + fibonaciSeries(n - 2);
     }
 
 }
