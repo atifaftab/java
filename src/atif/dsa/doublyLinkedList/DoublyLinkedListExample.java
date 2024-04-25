40f85503a50636d7b1b0e4b579b2636b76d13095
Atif <iamatif96@gmail.com>
2024-04-25 23:36:06 +0800

    basic methods for doubly LinkedList

--- src/atif/dsa/doublyLinkedList/DoublyLinkedListExample.java
+++ src/atif/dsa/doublyLinkedList/DoublyLinkedListExample.java
@@ -10,8 +10,35 @@ public class DoublyLinkedListExample {
         Node next;
         Node prev;
 
-        Node(int value) {
+        public Node(int value) {
             this.value = value;
         }
+    }
+
+    public DoublyLinkedListExample(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void printDoubleLinkedList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("length: " + length);
     }
 }
