2397e1bb751fad98b7b79a54224ea6f8e7684541
Atif <iamatif96@gmail.com>
2024-04-25 23:51:39 +0800

    append

---
+++ src/atif/dsa/append/Append.java
@@ -0,0 +1,56 @@
+package atif.dsa.append;
+
+public class Append {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Append(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("length: " + length);
+    }
+
+    public void append(int value) {
+        Node node = new Node(value);
+        if (length == 0) {
+            head = node;
+            tail = node;
+        }
+        tail.next = node;
+        node.next = tail;
+        tail = node;
+        length++;
+    }
+}
