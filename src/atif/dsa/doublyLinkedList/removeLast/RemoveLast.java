452953d8c1cef06fbab8219bf429e9e7cf0fc3a0
Atif <iamatif96@gmail.com>
2024-04-26 00:35:13 +0800

    remove Last

---
+++ src/atif/dsa/doublyLinkedList/removeLast/RemoveLast.java
@@ -0,0 +1,75 @@
+package atif.dsa.doublyLinkedList.removeLast;
+
+public class RemoveLast {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public RemoveLast(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void displayList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = tail.next;
+        }
+    }
+
+    public void append(int value) {
+        Node node = new Node(value);
+        if (length == 0) {
+            head = node;
+            tail = node;
+        }
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        pre.next = null;
+        temp.prev = null;
+        tail = pre;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    //better method
+    public Node removeLast1() {
+        if (length == 0) return null;
+        Node temp = tail;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+        length--;
+        return temp;
+    }
+}
