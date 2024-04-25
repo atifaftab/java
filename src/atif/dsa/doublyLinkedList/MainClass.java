ce1db285d20140eaff50a08b805326f56dee93f4
Atif <iamatif96@gmail.com>
2024-04-25 23:37:45 +0800

    basic methods for doubly LinkedList

---
+++ src/atif/dsa/doublyLinkedList/MainClass.java
@@ -0,0 +1,11 @@
+package atif.dsa.doublyLinkedList;
+
+public class MainClass {
+    public static void main(String[] args) {
+        DoublyLinkedListExample list = new DoublyLinkedListExample(5);
+        list.getHead();
+        list.getTail();
+        list.getLength();
+        list.printDoubleLinkedList();
+    }
+}
