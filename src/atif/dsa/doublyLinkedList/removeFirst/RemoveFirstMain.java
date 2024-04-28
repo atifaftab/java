caea5def3e841e3b2dd148c26b683da666595c27
Atif <iamatif96@gmail.com>
2024-04-28 16:32:13 +0800

    double linked list remove first

--- src/atif/dsa/doublyLinkedList/removeFirst/RemoveFirstMain.java
+++ src/atif/dsa/doublyLinkedList/removeFirst/RemoveFirstMain.java
@@ -3,6 +3,13 @@ package atif.dsa.doublyLinkedList.removeFirst;
 public class RemoveFirstMain {
     public static void main(String[] args) {
         RemoveFirst list = new RemoveFirst(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
         list.displayList();
+        System.out.println("----");
+        list.removeFirst();
+        list.displayList();
+
     }
 }
