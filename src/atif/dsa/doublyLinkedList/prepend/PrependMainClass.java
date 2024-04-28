373d05c617befc8dfed9c97df9fcc658a53119d5
Atif <iamatif96@gmail.com>
2024-04-28 15:42:53 +0800

    double linked list prepend

---
+++ src/atif/dsa/doublyLinkedList/prepend/PrependMainClass.java
@@ -0,0 +1,9 @@
+package atif.dsa.doublyLinkedList.prepend;
+
+public class PrependMainClass {
+    public static void main(String[] args) {
+        Prepend list = new Prepend(1);
+        list.displayList();
+    }
+
+}
