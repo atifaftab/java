8b5fd05aa2e25fff70dde04e10d867d19852d51d
Atif <iamatif96@gmail.com>
2024-04-28 16:03:24 +0800

    double linked list prepend

--- src/atif/dsa/doublyLinkedList/prepend/PrependMainClass.java
+++ src/atif/dsa/doublyLinkedList/prepend/PrependMainClass.java
@@ -4,6 +4,10 @@ public class PrependMainClass {
     public static void main(String[] args) {
         Prepend list = new Prepend(1);
         list.displayList();
+        System.out.println("---");
+        list.prepend(0);
+        list.prepend(-1);
+        list.displayList();
     }
 
 }
