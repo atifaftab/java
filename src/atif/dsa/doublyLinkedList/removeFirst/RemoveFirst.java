9a176d56cb0975e7676fe042957fb2288ecfa7fe
Atif <iamatif96@gmail.com>
2024-04-28 16:22:50 +0800

    double linked list remove first

---
+++ src/atif/dsa/doublyLinkedList/removeFirst/RemoveFirst.java
@@ -0,0 +1,49 @@
+package atif.dsa.doublyLinkedList.removeFirst;
+
+import atif.dsa.doublyLinkedList.removeLast.RemoveLast;
+
+public class RemoveFirst {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public RemoveFirst(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void displayList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node newHead = head.next;
+        head.next = null;
+        newHead.prev = null;
+        head = newHead;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        length--;
+        return temp;
+    }
+}
