c540e8fd61a3a952e2588166fe141855d943440b
Atif <iamatif96@gmail.com>
2024-04-30 22:28:29 +0800

    double linked list Get

--- src/atif/dsa/doublyLinkedList/get/Get.java
+++ src/atif/dsa/doublyLinkedList/get/Get.java
@@ -23,6 +23,15 @@ public class Get {
         tail = node;
         length = 1;
     }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
     public void append(int value) {
         Node node = new Node(value);
         if (length == 0) {
@@ -35,16 +44,17 @@ public class Get {
         tail = node;
         length++;
     }
-    public Node get(int index){
-        if(index<0 || index >=length) return null;
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
         Node temp = head;
-        if(index<length/2){
-            for(int i=0; i<index; i++){
+        if (index < length / 2) {
+            for (int i = 0; i < index; i++) {
                 temp = temp.next;
             }
-        }else {
-            temp =tail;
-            for(int i=length-1;i>index;i--){
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
                 temp = temp.prev;
             }
         }
