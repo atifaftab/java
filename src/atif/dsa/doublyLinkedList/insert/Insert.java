b354591e38e5617b24acf11fad1d1cad3085f06a
Atif <iamatif96@gmail.com>
2024-05-03 03:18:18 +0800

    double linked list insert

--- src/atif/dsa/doublyLinkedList/insert/Insert.java
+++ src/atif/dsa/doublyLinkedList/insert/Insert.java
@@ -1,14 +1,17 @@
 package atif.dsa.doublyLinkedList.insert;
 
+import atif.dsa.doublyLinkedList.set.Set;
+
 public class Insert {
     private Node head;
     private Node tail;
-    private Node prev;
     private int length;
 
     class Node {
         int value;
         Node next;
+        Node prev;
+
 
         public Node(int value) {
             this.value = value;
@@ -30,7 +33,32 @@ public class Insert {
         }
     }
 
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        if (index < length / 2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+
     public boolean insert(int index, int value) {
+        if(index==0){
+            append(value);
+        }
+        Node temp = get(index);
+        Node pre = temp(index-1);
+        if(temp!=null){
+            Node pre = temp.prev;
+        }
         return false;
     }
 }
