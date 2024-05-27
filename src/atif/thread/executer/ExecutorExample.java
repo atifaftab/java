3d18ab260a08fb619c058e112e90a48d13541290
Atif <iamatif96@gmail.com>
2024-05-28 06:44:11 +0800

    Executor interface Example

--- src/atif/thread/executer/ExecutorExample.java
+++ src/atif/thread/executer/ExecutorExample.java
@@ -1,10 +1,15 @@
 package atif.thread.executer;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ExecutorExample implements Runnable {
     public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 10; i++) {
-            Thread t1 = new Thread(new ExecutorExample());
-            t1.start();
+//            Thread t1 = new Thread(new ExecutorExample());
+//            t1.start();
+            service.execute(new ExecutorExample());
         }
         System.out.println("Thread Name: " + Thread.currentThread().getName());
     }
