bed1cc7dd9b013978922c0a8e9d16946e8ffbed6
Atif <iamatif96@gmail.com>
2024-06-07 21:03:31 +0800

    record example

---
+++ src/atif/java16/RecordExample.java
@@ -0,0 +1,9 @@
+package atif.java16;
+
+
+//record is an immutable final class
+public record RecordExample(int id, String name) {
+//    it generates
+//    A public constructor that initializes all components.
+//    Accessor methods for each component
+}
