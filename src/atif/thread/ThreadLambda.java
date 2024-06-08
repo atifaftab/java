c91fbb96d66bb0ba1f60191743186ae2c135be7f
Atif <iamatif96@gmail.com>
2024-06-08 21:15:02 +0800

    multithreading

--- src/atif/thread/ThreadLambda.java
+++ src/atif/thread/ThreadLambda.java
@@ -33,6 +33,16 @@ public class ThreadLambda {
                 }
             }
         });
+        Thread t4 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("4th thread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         t1.start();
         try {
@@ -45,5 +55,6 @@ public class ThreadLambda {
 //        t2.join();
         System.out.println("bye");
         t3.start();
+        t4.start();
     }
 }
