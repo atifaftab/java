182e37177285abb642ee06b8012ebb73c8e30b7b
Atif <iamatif96@gmail.com>
2024-06-09 09:15:23 +0800

    max trailing diff with efficient solution

--- src/array/MaxTrailingDiff.java
+++ src/array/MaxTrailingDiff.java
@@ -2,19 +2,31 @@ package array;
 
 public class MaxTrailingDiff {
     public static void main(String[] args) {
-        int[] num = {9, 1, 86, 7, 343,342, 5, 1};
+        int[] num = {9, 1, 86, 7, 343, 342, 5, 1};
         int n = num.length;
-        int min = num[1] - num[0];
-//        int maxDiff = 0;
+        int maxDiff = num[1] - num[0];
+
+        //brute force
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i + 1; j < n; j++) {
+//                if (num[j] - num[i] > maxDiff) {
+//                    maxDiff = num[j] - num[i];
+//                }
+//            }
+//        }
+//        System.out.println(maxDiff);
+
+
+        //efficient way
+        int min = num[0];
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (num[j] - num[i] > min) {
-                    min = num[j] - num[i];
-                }
-            }
+            if (num[i] - min > maxDiff)
+                maxDiff = num[i] - min;
+            if (num[i] < min)
+                min = num[i];
         }
-        System.out.println(min);
+        System.out.println(maxDiff);
     }
 
-//    for(int i=0; i<n)
+
 }
