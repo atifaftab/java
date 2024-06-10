b8ed75c10a791727ca4ee982b11b1f9fe0371a50
Atif <iamatif96@gmail.com>
2024-06-11 06:28:57 +0800

    minor code improvement

--- src/atif/java8/lambda/LambdaDemo2.java
+++ src/atif/java8/lambda/LambdaDemo2.java
@@ -4,11 +4,18 @@ interface Home {
     void mood(int num);
 }
 
+interface Demo {
+    void print(String name);
+}
+
 public class LambdaDemo2 {
 
     public static void main(String[] args) {
         Home obj;
         obj = i -> System.out.println("Number is " + i);
         obj.mood(10);
+
+        Demo demo = str -> System.out.println(str);
+        demo.print("Atif");
     }
 }
