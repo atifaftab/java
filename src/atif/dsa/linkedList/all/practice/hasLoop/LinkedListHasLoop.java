619c8748126633178a47a877d8b44bd62fd18da6
Atif <iamatif96@gmail.com>
2024-08-16 07:27:27 +0800

    LinkedList : has loop

---
+++ src/atif/dsa/linkedList/all/practice/hasLoop/LinkedListHasLoop.java
@@ -0,0 +1,59 @@
+package atif.dsa.linkedList.all.practice.hasLoop;
+
+public class LinkedListHasLoop {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        Node next;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedListHasLoop(int value) {
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public void allPrint() {
+        if (head == null) {
+            System.out.println("head: null");
+            System.out.println("tail: null");
+        } else {
+            System.out.println("head: " + head.value);
+            System.out.println("tail: " + tail.value);
+        }
+        System.out.println("\nLinkedList");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void append(int value) {
+        Node node = new Node(value);
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+        }
+        tail = node;
+        length++;
+    }
+}
---
+++ src/atif/dsa/linkedList/all/practice/hasLoop/LinkedListHasLoopMain.java
@@ -0,0 +1,12 @@
+package atif.dsa.linkedList.all.practice.hasLoop;
+
+public class LinkedListHasLoopMain {
+    public static void main(String[] args) {
+        LinkedListHasLoop list = new LinkedListHasLoop(0);
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.allPrint();
+    }
+
+}
