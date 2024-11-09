467fefe77f84d4501e15c55c05c04cb62e58fcf2
Atif <iamatif96@gmail.com>
2024-11-09 18:14:15 +0800

    Queue

---
+++ src/atif/dsa/queue/QueueDemo.java
@@ -0,0 +1,40 @@
+package atif.dsa.queue;
+
+public class QueueDemo {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public QueueDemo(int value) {
+        Node node = new Node(value);
+        this.first = node;
+        this.last = node;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        System.out.println("QUEUE:");
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printFirst() {
+        System.out.println("first: " + first);
+    }
+
+    public void printLast() {
+        System.out.println("Last: " + last);
+    }
+}
---
+++ src/atif/dsa/queue/QueueDemoMain.java
@@ -0,0 +1,8 @@
+package atif.dsa.queue;
+
+public class QueueDemoMain {
+    public static void main(String[] args) {
+        QueueDemo queue = new QueueDemo(5);
+        queue.printQueue();
+    }
+}
