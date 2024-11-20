d1586520da3a7b860dfda647e785f6c6b0d36c15
Atif <iamatif96@gmail.com>
2024-11-20 21:34:11 +0800

    largest number

--- src/atif/arrays/LargestNumber.java
+++ src/atif/arrays/LargestNumber.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 
 public class LargestNumber {
     public static void main(String[] args) {
-        int[] num = new int[]{11,25,4,36,95};
-
+        int[] num = new int[]{11, 25, 4, 36, 95};
         //naive way
 //        int max = num[0];
 //        for(Integer n: num){
@@ -17,6 +16,9 @@ public class LargestNumber {
         //using sort
 //        Arrays.stream(num).sorted();
 //        System.out.println(num[num.length-1]);
+
+//        Arrays.sort(num);
+//        System.out.println(num[num.length - 1]);
 
         //latest  java 8
         System.out.println(Arrays.stream(num).max().getAsInt());
