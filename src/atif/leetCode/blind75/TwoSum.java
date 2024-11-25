6dc8a88a3f6282545d66c724df24a762ce44259f
Atif <iamatif96@gmail.com>
2024-11-25 16:22:49 +0800

    two sum

---
+++ src/atif/leetCode/blind75/TwoSum.java
@@ -0,0 +1,32 @@
+package atif.leetCode.blind75;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+        Example 1:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].*/
+public class TwoSum {
+    public static void main(String[] args) {
+//        int[] nums = {2, 7, 11, 15};
+        int[] nums = {5, 7, 4, 15, 8, 34};
+        int target = 23;
+        System.out.println(Arrays.toString(twoSumMethod(nums, target)));
+    }
+
+    private static int[] twoSumMethod(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff))
+                return new int[]{map.get(diff), i};
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
