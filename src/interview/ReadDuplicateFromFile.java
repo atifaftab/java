7221d0bc9eacfc17386e53bd3682000639c05f29
Atif <iamatif96@gmail.com>
2024-11-25 18:09:09 +0800

    file with duplicate names using streams

--- src/interview/ReadDuplicateFromFile.java
+++ src/interview/ReadDuplicateFromFile.java
@@ -1,15 +1,17 @@
 package interview;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
 //File containing names separated by commas.
 // provide a Java program that finds all duplicate names in the file and counts the occurrences of each
 public class ReadDuplicateFromFile {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filePath = "C:\\work\\workspaces\\spring boot\\javaPractice\\src\\interview\\DuplicateNames.txt";
-        Map<String, Integer> duplicateNamesAndCountMap = new HashMap<>();
+       /* Map<String, Integer> duplicateNamesAndCountMap = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line = "";
             while ((line = br.readLine()) != null) {
@@ -27,6 +29,19 @@ public class ReadDuplicateFromFile {
         duplicateNamesAndCountMap.forEach((names, count) -> {
             if (count > 1)
                 System.out.println(names + " : " + count);
+        });*/
+
+
+        // using streams
+
+        Map<String, Long> namesMap = Files.lines(Paths.get(filePath))
+                .flatMap(line -> Arrays.stream(line.split(",")))
+                .map(String::trim)
+                .collect(Collectors.groupingBy(name -> name, Collectors.counting()));
+
+        namesMap.forEach((names, count) -> {
+            if (count > 1)
+                System.out.println(names + ": " + count);
         });
     }
 }
