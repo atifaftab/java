96b3de71fcf65a4a41c86c38c3c95cb05a72764a
Atif <iamatif96@gmail.com>
2024-12-03 21:43:44 +0800

    Contains Duplicate in int array using Set

--- src/atif/leetCode/blind75/ContainsDuplicate.java
+++ src/atif/leetCode/blind75/ContainsDuplicate.java
@@ -1,9 +1,16 @@
 package atif.leetCode.blind75;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] num = new int[]{2, 45, 36, 78, 2, 1, 9, 4};
+        int[] num = new int[]{2, 45, 2, 36, 78, 1, 9, 4};
         System.out.println(containsDuplicate(num));
+        System.out.println("===== using set ====");
+        System.out.println(containsDuplicate2(num));
     }
 
     //brute force
@@ -13,6 +20,15 @@ public class ContainsDuplicate {
                 if (num[i] == num[j])
                     return true;
             }
+        }
+        return false;
+    }
+
+    private static boolean containsDuplicate2(int[] num) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < num.length; i++) {
+            if (!set.add(num[i]))
+                return true;
         }
         return false;
     }
